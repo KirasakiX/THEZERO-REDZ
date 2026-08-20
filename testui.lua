af620665fe0ad3143a234af7e689060d23f2d55f
KirasakiX <thinhgia877@gmail.com>
2026-08-20 18:10:53 +0700

    Remove button section from Tab Info

--- testui.lua
+++ testui.lua
@@ -2,12 +2,3 @@ local Library = loadstring(game:HttpGet("https://raw.githubusercontent.com/Kiras
 
 local Window = Library:CreateWindow({ Title = "Vela Hub - Blox Fruit" })
 
-local Tab = Window:AddTab("Tab Info")
-local Section = Tab:AddSection("Chức Năng", false)
-
-Section:AddButton({
-    Title = "Bấm Vào Đây",
-    Callback = function()
-        print("Đã chạy!")
-    end
-})
