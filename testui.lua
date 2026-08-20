4c272d443bd26b7b0b47040e9b58694b53c896f5
KirasakiX <thinhgia877@gmail.com>
2026-08-20 19:03:04 +0700

    Delete testui.lua

--- testui.lua
+++
@@ -1,4 +0,0 @@
-local Library = loadstring(game:HttpGet("https://raw.githubusercontent.com/KirasakiX/THEZERO-REDZ/refs/heads/main/uibnaana.lua))()
-
-local Window = Library:CreateWindow({ Title = "Vela Hub - Blox Fruit" })
-
